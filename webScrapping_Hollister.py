510942ecb8b738fa6915b6dae33f5ebf4c6e7949
Mauricio Revuelta <mauriciorevueltas@gmail.com>
2024-03-26 11:35:55 -0400

    Update webScrapping_Hollister.py

--- webScrapping_Hollister.py
+++ webScrapping_Hollister.py
@@ -1,16 +1,21 @@
+# importing required libraries 
 import requests
 from bs4 import BeautifulSoup
 
-#url of the air Jordan products
-
+# Targets the URL for the specific website
 URL = "https://www.hollisterco.com/shop/ca/mens"
+
 r = requests.get(URL)
 
 soup = BeautifulSoup(r.content, 'html5lib')
 
+# Access the div tag with the product name
 div_product_name = soup.find_all('div',attrs={'class':'product-name'})
+
+# Access the span tag with the product price
 span_product_price = soup.find_all('span',attrs={'class':'product-price'})
 
+# Loop to find and print all the products names and price 
 for name, price in zip(div_product_name, span_product_price):
     product_name = name.find('h2')
     product_price = price.find('span')
